16790fb2595be9c56b2f0bc053b3382e1e337c9f
Ben Moss <ben@mossity.com>
2023-10-10 12:05:29 -0400

    new next app

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className='bg-green-300 border-green-600 border-b p-4 m-4 rounded'>
+    hello world
+    </main>
+  )
+}
