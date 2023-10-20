c2341e5a8b2d2a55b7a53315515cbbdada37e6fb
Ben Moss <ben@mossity.com>
2023-10-20 12:12:47 -0400

    futzing with html

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,17 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className='bg-green-300 border-green-600 border-b p-4 m-4 rounded'>
-    hello world
-    </main>
+    <div className='min-h-screen overflow-hidden'>
+      <div className='relative mt-16 overflow-hidden md:mt-18 bg-gradient-to-b from-gray-50 to-white'>
+        <div className='relative pb-4'>
+          <nav className='backdrop-filter backdrop-blur-xl border-b border-slate-900/5 z-10 bg-gray-50/90 fixed top-0 w-full'>
+            nav here
+          </nav>
+          <main className='px-4 mx-auto mt-10 max-w-7xl sm:mt-14'>
+            main here
+          </main>
+        </div>
+      </div>
+    </div>
   )
 }
